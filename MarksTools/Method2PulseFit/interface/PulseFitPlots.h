6dcd068b32066cd7d9cf52fc1111d4f724f95710
Mark Grimes <mark.grimes@cern.ch>
2015-01-14 00:59:52 +0100

    Change interface to take FittedPulse

--- MarksTools/Method2PulseFit/interface/PulseFitPlots.h
+++ MarksTools/Method2PulseFit/interface/PulseFitPlots.h
@@ -8,6 +8,10 @@
 //
 class DQMStore;
 class MonitorElement;
+namespace markstools
+{
+	class FittedPulse;
+}
 
 namespace markstools
 {
@@ -20,18 +24,13 @@ namespace markstools
 	public:
 		PulseFitPlots( DQMStore* pDQMStore );
 		~PulseFitPlots();
-		void fill( const std::vector<double>& fitParsVec );
+		void fill( const markstools::FittedPulse& pulse );
 	protected:
 		MonitorElement* pChargeValFit_;
 		MonitorElement* pTimeValFit_;
 		MonitorElement* pPedValFit_;
 		MonitorElement* pChi2_;
-		MonitorElement* pNaNInFit_;
-		std::pair<double,double> chargeValFitLimits_;
-		std::pair<double,double> timeValFitLimits_;
-		std::pair<double,double> pedValFitLimits_;
-		std::pair<double,double> chi2Limits_;
-		std::pair<double,double> NaNInFitLimits_;
+		MonitorElement* pDeltaTime_;
 	};
 
 } // end of namespace markstools
--- MarksTools/Method2PulseFit/src/PulseFitPlots.cc
+++ MarksTools/Method2PulseFit/src/PulseFitPlots.cc
@@ -1,47 +1,45 @@
 #include "MarksTools/Method2PulseFit/interface/PulseFitPlots.h"
 
+#include "MarksTools/Method2PulseFit/interface/FittedPulse.h"
 #include <FWCore/ServiceRegistry/interface/Service.h>
 #include <DQMServices/Core/interface/DQMStore.h>
 #include <DQMServices/Core/interface/MonitorElement.h>
 
-markstools::PulseFitPlots::PulseFitPlots( DQMStore* pDQMStore ) :
-		chargeValFitLimits_( 999999, -999999 ), timeValFitLimits_( 999999, -999999 ), pedValFitLimits_( 999999, -999999 ), chi2Limits_( 999999, -999999 ), NaNInFitLimits_( 999999, -999999 )
+markstools::PulseFitPlots::PulseFitPlots( DQMStore* pDQMStore )
 {
 	pChargeValFit_=pDQMStore->book1D( "ChargeValFit", "ChargeValFit", 200, 0, 200 );
 	pTimeValFit_=pDQMStore->book1D( "TimeValFit", "TimeValFit", 200, -100, 100 );
 	pPedValFit_=pDQMStore->book1D( "PedValFit", "PedValFit", 200, -1, 5 );
 	pChi2_=pDQMStore->book1D( "Chi2", "Chi2", 200, 0, 22000 );
-	pNaNInFit_=pDQMStore->book1D( "NaNInFit", "NaNInFit", 200, 0, 100 );
+	pDeltaTime_=pDQMStore->book2D( "deltaTime", "deltaTime", 50, 0, 100, 50, 0, 100 );
 }
 
 markstools::PulseFitPlots::~PulseFitPlots()
 {
-	std::cout << "----- chargeValFitLimits_ " << chargeValFitLimits_.first << " " << chargeValFitLimits_.second << "\n"
-			<< "----- timeValFitLimits_ " << timeValFitLimits_.first << " " << timeValFitLimits_.second << "\n" << "----- pedValFitLimits_ "
-			<< pedValFitLimits_.first << " " << pedValFitLimits_.second << "\n" << "----- chi2Limits_ " << chi2Limits_.first << " "
-			<< chi2Limits_.second << "\n" << "----- NaNInFitLimits_ " << NaNInFitLimits_.first << " " << NaNInFitLimits_.second << std::endl;
+	// No operation
 }
 
-void markstools::PulseFitPlots::fill( const std::vector<double>& fitParsVec )
+void markstools::PulseFitPlots::fill( const markstools::FittedPulse& pulse )
 {
-	if( fitParsVec[0] < chargeValFitLimits_.first ) chargeValFitLimits_.first=fitParsVec[0];
-	if( fitParsVec[0] > chargeValFitLimits_.second ) chargeValFitLimits_.second=fitParsVec[0];
+	if( pulse.chosenPulse_!=-1 ) // If a pulse was successfully chosen
+	{
+		pChargeValFit_->Fill( pulse.fittedPulses_[pulse.chosenPulse_].second );
+		pTimeValFit_->Fill( pulse.fittedPulses_[pulse.chosenPulse_].first );
 
-	if( fitParsVec[1] < timeValFitLimits_.first ) timeValFitLimits_.first=fitParsVec[1];
-	if( fitParsVec[1] > timeValFitLimits_.second ) timeValFitLimits_.second=fitParsVec[1];
+		if( pulse.fittedPulses_.size()==3 ) // If it was a 3 pulse fit, plot the difference in time between the pulses
+		{
+			std::vector<double> timeDifferences;
+			for( size_t index=0; index<pulse.fittedPulses_.size(); ++index )
+			{
+				if( static_cast<int>(index)==pulse.chosenPulse_ ) continue; // don't work out difference to itself
+				timeDifferences.push_back( std::abs(pulse.fittedPulses_[pulse.chosenPulse_].first-pulse.fittedPulses_[index].first) );
+			}
+			// Presumably this vector has only 2 entries. Sort in ascending order.
+			std::sort( timeDifferences.begin(), timeDifferences.end() );
 
-	if( fitParsVec[2] < pedValFitLimits_.first ) pedValFitLimits_.first=fitParsVec[2];
-	if( fitParsVec[2] > pedValFitLimits_.second ) pedValFitLimits_.second=fitParsVec[2];
-
-	if( fitParsVec[3] < chi2Limits_.first ) chi2Limits_.first=fitParsVec[3];
-	if( fitParsVec[3] > chi2Limits_.second ) chi2Limits_.second=fitParsVec[3];
-
-	if( fitParsVec[4] < NaNInFitLimits_.first ) NaNInFitLimits_.first=fitParsVec[4];
-	if( fitParsVec[4] > NaNInFitLimits_.second ) NaNInFitLimits_.second=fitParsVec[4];
-
-	pChargeValFit_->Fill( fitParsVec[0] );
-	pTimeValFit_->Fill( fitParsVec[1] );
-	pPedValFit_->Fill( fitParsVec[2] );
-	pChi2_->Fill( fitParsVec[3] );
-	pNaNInFit_->Fill( fitParsVec[4] );
+			pDeltaTime_->Fill( timeDifferences[0], timeDifferences[1] );
+		}
+	}
+	pPedValFit_->Fill( pulse.fittedPedestal_ );
+	pChi2_->Fill( pulse.chi2_ );
 }
