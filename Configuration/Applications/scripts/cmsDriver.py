a26d801ee711b1bc0a7bc67e4b8505ed1bdc9325
Mark Grimes <mark.grimes@cern.ch>
2015-10-15 00:09:15 +0200

    Unload modules before dump, so that era commands are applied.

--- Configuration/Applications/scripts/cmsDriver.py
+++ Configuration/Applications/scripts/cmsDriver.py
@@ -8,6 +8,7 @@ def run():
         import Configuration.Applications
         from Configuration.Applications.ConfigBuilder import ConfigBuilder
         from Configuration.Applications.cmsDriverOptions import OptionsFromCommandLine
+        basicModules = sys.modules.keys() # Make a note of the modules before doing any configuration
         options = OptionsFromCommandLine()
         
         # after cleanup of all config parameters pass it to the ConfigBuilder
@@ -20,6 +21,13 @@ def run():
 
         # handle different dump options
         if options.dump_python:
+            # Need to unload any modules loaded during the previous configuration parse,
+            # otherwise any statements in the modules will not be run on the new file
+            # (python only executes them on first import). This means any era commands
+            # will not be applied when loading the file back in.
+            for moduleName in sys.modules.keys() :
+                if not moduleName in basicModules :
+                    del sys.modules[moduleName]
             result = {}
             execfile(options.python_filename, result)
             process = result["process"]
